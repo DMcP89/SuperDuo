af534785ae3fb82b28a9cb3b8c7b1458ad8a02e9
Dave McPherson <dmcp89@gmail.com>
2015-11-04 20:50:16 -0500

    building out widget

--- Football_Scores/app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetProvider.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetProvider.java
@@ -1,9 +1,32 @@
 package barqsoft.footballscores.widget;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
 
 /**
  * Created by dave on 10/23/2015.
  */
 public class ScoresWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
 }
